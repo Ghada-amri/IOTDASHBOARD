fc398f0024845822d181e118cc733f6f27603604
ghada <ghadaamri80@gmail.com>
2019-03-17 12:28:54 +0100

    Mock to dynamic data

--- src/app/@core/services/temperature-humidity.service.ts
+++ src/app/@core/services/temperature-humidity.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { of as observableOf,  Observable } from 'rxjs';
+import { of as observableOf,  Observable, from } from 'rxjs';
 import { TemperatureHumidityData, Temperature } from '../data/temperature-humidity';
-
+import {Http} from "@angular/http";
 @Injectable()
 export class TemperatureHumidityService extends TemperatureHumidityData {
 
@@ -16,9 +16,13 @@ export class TemperatureHumidityService extends TemperatureHumidityData {
     min: 0,
     max: 100,
   };
+  constructor(private http: Http) {
+      super();
+  }
 
   getTemperatureData(): Observable<Temperature> {
     return observableOf(this.temperatureDate);
+    //this.http.get("url-to-firebase").subscribe();
   }
 
   getHumidityData(): Observable<Temperature> {
